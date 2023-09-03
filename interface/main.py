89153cf35d4818d36a48f6c19bcedcbea4151149
yassir benjilali <benjilaliyassir@forleaders.net>
2023-09-03 23:09:35 +0100

    debug

--- interface/main.py
+++ interface/main.py
@@ -83,7 +83,7 @@ with st.form("input_form",clear_on_submit=True):
         if input_img is not None:
             result=[]
             result_proba=1
-            cv2.imwrite(input_img,"image_input")
+            cv2.imwrite("image_input",input_img)
             st.image(input_img)
             st.text(input_img.shape)
             image_brute=cv2.imread("image_input")
