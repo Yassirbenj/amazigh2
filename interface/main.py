e6f6524f4660a55edbf5c8d165e5163751b7e013
yassir benjilali <benjilaliyassir@forleaders.net>
2023-09-04 23:28:33 +0100

    debug

--- interface/main.py
+++ interface/main.py
@@ -43,6 +43,7 @@ def preprocess(image):
     #st.image(gray_image)
     thresh = cv2.threshold(image_rescale, 0, 255, cv2.THRESH_BINARY_INV)[1]
     st.image(thresh)
+    st.text(thresh)
     contours,hierarchy=cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
